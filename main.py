1908d87ed28d550a37a9c1d63e12a5f541f12188
Sahil Shaikh <sahilsk565678@gmail.com>
2025-04-06 17:21:38 +0530

    convert port as integer

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import uvicorn
 import os
 
 if __name__ == "__main__":
-    port = os.getenv("PORT", 10000)
+    port = int(os.getenv("PORT", 10000))
     uvicorn.run("core.server:app", port=port, reload=True)
